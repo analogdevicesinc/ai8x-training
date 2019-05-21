b34ffb5bdfdc7746f472d42f516415d0ef3785ff
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-05-21 17:11:53 -0500

    AI84Net

--- ai84net.py
+++ ai84net.py
@@ -1,325 +1,76 @@
 """
-Modified from
-https://github.com/pytorch/vision/tree/master/torchvision/models
+Networks that fit into AI84
 """
 import torch.nn as nn
 
 
-__all__ = ['SResNet', 'sresnet8', 'SSResNet', 'sresnet6', 'SSSResNet', 'sresnet4',
-           'LResNet', 'lresnet18']
+__all__ = ['AI84Net5', 'ai84net5']
+
+AI84_WEIGHT_INPUTS = 64
+AI84_WEIGHT_DEPTH = 128
 
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+class AI84Net5(nn.Module):
 
+    def __init__(self, num_classes=10, num_channels=3, dimensions=(28, 28), planes=60, pool=4,
+                 fc_inputs=12, bias=False):
+        super(AI84Net5, self).__init__()
 
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+        # AI84 Limits
+        assert planes + num_channels <= AI84_WEIGHT_INPUTS
+        assert planes + fc_inputs <= AI84_WEIGHT_DEPTH-1
+        assert pool < 5
+        assert dimensions[0] == dimensions[1]
 
+        # Keep track of image dimensions so one constructor works for all image sizes
+        dim = dimensions[0]
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.conv1 = nn.Conv2d(num_channels, planes, kernel_size=3,
+                               stride=1, padding=2, bias=bias)
+        dim += 2  # padding -> 30x30
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = conv1x1(inplanes, planes)
-        self.conv2 = conv3x3(planes, planes, stride)
-        self.conv3 = conv1x1(planes, planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class SResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, num_channels=3,
-                 kernel_size=3):
-        super(SResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=kernel_size, stride=1, padding=2,
-                               bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(256 * block.expansion, num_classes)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1 if pool == 3 else 0)
+        if pool != 3:
+            dim -= 2  # stride of 2 -> 14x14, else 15x15
+        dim //= 2
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1 if pool == 3 else 2, bias=bias)
+        if pool != 3:
+            dim += 2  # padding 2 -> 16x16, else 15x15
+        self.conv3 = nn.Conv2d(planes, AI84_WEIGHT_DEPTH-planes-fc_inputs, kernel_size=3,
+                               stride=1, padding=1, bias=bias)
+        # no change in dimensions
+        self.avgpool = nn.AvgPool2d(pool)
+        dim //= pool  # pooling -> 4x4, else 3x3 or 5x5
+        self.conv4 = nn.Conv2d(AI84_WEIGHT_DEPTH-planes-fc_inputs, fc_inputs, kernel_size=3,
+                               stride=1, padding=1, bias=bias)
+        # no change in dimensions
+        self.fc = nn.Linear(fc_inputs*dim*dim, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.relu(x)
         x = self.avgpool(x)
+        x = self.conv4(x)
+        x = self.relu(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
         return x
 
 
-def sresnet8(pretrained=False, **kwargs):
-    """Constructs a ResNet-8 model.
-
+def ai84net5(pretrained=False, **kwargs):
+    """
+    Constructs a AI84Net-5 model.
     """
     assert not pretrained
-    return SResNet(BasicBlock, [1, 1, 1], **kwargs)
-
-
-class SSResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, num_channels=3,
-                 kernel_size=3):
-        super(SSResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=kernel_size, stride=1, padding=2,
-                               bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(128 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
-
-
-def sresnet6(pretrained=False, **kwargs):
-    """Constructs a ResNet-6 model.
-
-    """
-    assert not pretrained
-    return SSResNet(BasicBlock, [1, 1], **kwargs)
-
-
-class SSSResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, num_channels=3,
-                 kernel_size=3):
-        super(SSSResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=kernel_size, stride=1, padding=2,
-                               bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(64 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
-
-
-def sresnet4(pretrained=False, **kwargs):
-    """Constructs a ResNet-4 model.
-
-    """
-    assert not pretrained
-    return SSSResNet(BasicBlock, [1], **kwargs)
-
-
-class LResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, num_channels=3):
-        super(LResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=3, stride=1, padding=2,
-                               bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.dropout = nn.Dropout(p=0.5)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.dropout(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
-
-
-def lresnet18(pretrained=False, **kwargs):
-    """Constructs a ResNet-18 model with dropout.
-    """
-    assert not pretrained
-    return LResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    return AI84Net5(**kwargs)
