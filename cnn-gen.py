f33d93e04b1f43ec296dd5aa8ae91a7adf9a5ec7
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-06 11:22:32 -0500

    Break out hardware constants to separate file

--- cnn-gen.py
+++ cnn-gen.py
@@ -20,35 +20,11 @@ import torch
 import yaml
 
 import sampledata
+from ai84cnn import APB_BASE, C_MAX_LAYERS, MAX_LAYERS, TRAM_SIZE, BIAS_SIZE, MASK_WIDTH, \
+    C_CNN, C_CNN_BASE, C_TRAM_BASE, C_MRAM_BASE, C_BRAM_BASE, C_SRAM_BASE, C_TILE_OFFS, \
+    P_NUMTILES, P_NUMPRO, P_SHARED, INSTANCE_SIZE, TILE_SIZE, MEM_SIZE, MAX_CHANNELS
 from simulate import cnn_layer
 from utils import argmin, ffs, fls, popcount
-
-
-# AI84
-APB_BASE = 0x50100000
-
-# CNN hardware parameters
-C_MAX_LAYERS = 16
-MAX_LAYERS = 32
-TRAM_SIZE = 256
-BIAS_SIZE = 256
-MASK_WIDTH = 128
-C_CNN = 4
-C_CNN_BASE = 0
-C_TRAM_BASE = C_CNN_BASE + 0x800
-C_MRAM_BASE = C_CNN_BASE + 0x4800
-C_BRAM_BASE = C_CNN_BASE + 0xC800
-C_SRAM_BASE = C_CNN_BASE + 0x10000
-C_TILE_OFFS = 0x100000
-P_NUMTILES = 4
-P_NUMPRO = 16  # Processors per tile
-P_SHARED = 4  # Processors sharing a data memory
-
-INSTANCE_SIZE = 1024  # x32
-TILE_SIZE = 0x40000
-MEM_SIZE = INSTANCE_SIZE*P_NUMPRO*P_NUMTILES//P_SHARED  # x32
-MAX_DATA_DIM = 512  # Max theoretical size of single channel, used for command line validation only
-MAX_CHANNELS = P_NUMPRO*P_NUMTILES
 
 
 def s2u(i):
