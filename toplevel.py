0ff16fc9a85881d8bd20e6e563563ab209acbe07
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-10 11:46:08 -0500

    Modify generated C code

--- toplevel.py
+++ toplevel.py
@@ -16,14 +16,15 @@ def header(memfile, apb_base):
     Write include files and forward definitions to .c file handle `memfile`.
     The APB base address is passed in `apb_base`.
     """
-    memfile.write('#include "global_functions.h"\n')
+    memfile.write('#include "global_functions.h" // For RTL Simulation\n')
     memfile.write('#include <stdlib.h>\n')
     memfile.write('#include <stdint.h>\n')
-    memfile.write('#include <string.h>\n\n')
+    memfile.write('#include <string.h>\n')
+    memfile.write('#include "tornadocnn.h"\n\n')
 
     memfile.write('void cnn_wait(void)\n{\n')
     memfile.write(f'  while ((*((volatile uint32_t *) 0x{apb_base + C_CNN_BASE:08x}) '
-                  '& (1<<12)) != 1<<12) ;\n}\n\n')
+                  '& (1<<12)) != 1<<12) ;\n  CNN_COMPLETE; // Signal processing complete\n}\n\n')
 
 
 def load_header(memfile):
@@ -37,7 +38,7 @@ def load_footer(memfile):
     """
     Write the footer for the CNN configuration loader function to `memfile`.
     """
-    memfile.write('  return 1;\n}\n\n')
+    memfile.write('\n  CNN_START; // Allow capture of processing time\n\n  return 1;\n}\n\n')
 
 
 def main(memfile, classification_layer=False):
@@ -82,6 +83,7 @@ def fc_layer(memfile, weights, bias):
     memfile.write('}\nstatic const q7_t fc_weights[FC_OUT * FC_IN] = FC_WEIGHTS;\n\n')
 
     memfile.write('static uint8_t conv_data[FC_IN];\n')
+    memfile.write('static q15_t fc_buffer[FC_IN];\n')
     memfile.write('static q15_t fc_output[FC_OUT];\n\n')
 
     if bias is not None:
@@ -89,13 +91,13 @@ def fc_layer(memfile, weights, bias):
         bias.tofile(memfile, sep=',', format='%d')
         memfile.write('}\nstatic const q7_t fc_bias[FC_OUT] = FC_BIAS;\n\n')
 
-    memfile.write('void fc_layer(void)\n'
+    memfile.write('int fc_layer(void)\n'
                   '{\n  unload(conv_data);\n')
 
-    memfile.write('  arm_fully_connected_mat_q7_vec_q15((q7_t *) conv_data, fc_weights, '
+    memfile.write('  arm_fully_connected_q7_q15((q7_t *) conv_data, fc_weights, '
                   'FC_IN, FC_OUT, 0, 0, '
                   f'{"fc_bias" if bias is not None else "NULL"}, '
-                  'fc_output, NULL);\n')
+                  'fc_output, fc_buffer);\n')
     # arm_softmax_q7(output_data, 10, output_data);
 
     memfile.write('  return 1;\n}\n\n')
@@ -109,6 +111,6 @@ def fc_verify(memfile, data):
     memfile.write('#define FC_EXPECTED {')
     data.tofile(memfile, sep=',', format='%d')
     memfile.write('}\nstatic q15_t fc_expected[FC_OUT] = FC_EXPECTED;\n\n')
-    memfile.write('void fc_verify(void)\n'
+    memfile.write('int fc_verify(void)\n'
                   '{\n')
-    memfile.write('  return memcmp(fc_output, fc_expected, FC_OUT * sizeof(q15_t));\n}\n\n')
+    memfile.write('  return memcmp(fc_output, fc_expected, FC_OUT * sizeof(q15_t)) == 0;\n}\n\n')
---
+++ tornadocnn.h
@@ -0,0 +1,18 @@
+/*
+ * On-device execution
+ */
+
+#include <arm_math.h>
+#include <arm_nnfunctions.h>
+
+#define CNN_START LED_On(0)
+#define CNN_COMPLETE LED_Off(0)
+
+arm_status
+arm_fully_connected_q7_q15(const q7_t * pV,
+                           const q7_t * pM,
+                           const uint16_t dim_vec,
+                           const uint16_t num_of_rows,
+                           const uint16_t bias_shift,
+                           const uint16_t out_shift, const q7_t * bias, q15_t * pOut, q15_t * vec_buffer);
+
