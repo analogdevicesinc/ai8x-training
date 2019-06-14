dcc2413aa9404ae29118bf27fee03d028b0c33e5
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-14 15:36:33 -0500

    Enable bias for FC layer

--- ai84net.py
+++ ai84net.py
@@ -54,7 +54,7 @@ class AI84Net5(nn.Module):
         dim //= pool  # pooling, padding 0 -> 4x4
         # padding 1 -> no change in dimensions
 
-        self.fc = ai84.SoftwareLinear(fc_inputs*dim*dim, num_classes, simulate=simulate)
+        self.fc = ai84.SoftwareLinear(fc_inputs*dim*dim, num_classes, bias=True, simulate=simulate)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
