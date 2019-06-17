97ad46414ab5af1db9861af6e5de104dc0a2bcad
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-17 17:08:27 -0500

    AI85 changes

--- tornadocnn.py
+++ tornadocnn.py
@@ -17,14 +17,28 @@ MAX_LAYERS = 32
 TRAM_SIZE = 256
 BIAS_SIZE = 256
 MASK_WIDTH = 128
-MCNT_SAD_OFFS = 8
+
+MCNT_SAD_OFFS_AI84 = 8
+MCNT_SAD_OFFS_AI85 = 16
+MCNT_SAD_OFFS = MCNT_SAD_OFFS_AI84
+
 C_CNN = 4
 C_CNN_BASE = 0
 C_TRAM_BASE = C_CNN_BASE + 0x800
-C_MRAM_BASE = C_CNN_BASE + 0x4800
-C_BRAM_BASE = C_CNN_BASE + 0xC800
-C_SRAM_BASE = C_CNN_BASE + 0x10000
-C_GROUP_OFFS = 0x100000
+C_MRAM_BASE_AI84 = C_CNN_BASE + 0x4800
+C_MRAM_BASE_AI85 = C_CNN_BASE + 0x10000
+C_MRAM_BASE = C_MRAM_BASE_AI84
+C_BRAM_BASE_AI84 = C_CNN_BASE + 0xC800
+C_BRAM_BASE_AI85 = C_CNN_BASE + 0x5000
+C_BRAM_BASE = C_BRAM_BASE_AI84
+C_SRAM_BASE_AI84 = C_CNN_BASE + 0x10000
+C_SRAM_BASE_AI85 = C_CNN_BASE + 0x300000
+C_SRAM_BASE = C_SRAM_BASE_AI84
+
+C_GROUP_OFFS_AI84 = 0x100000
+C_GROUP_OFFS_AI85 = 0x400000
+C_GROUP_OFFS = C_GROUP_OFFS_AI84
+
 P_NUMGROUPS = 4
 P_NUMPRO = 16  # Processors per group
 P_SHARED = 4  # Processors sharing a data memory
@@ -53,13 +67,14 @@ LREG_LCTL = 9
 LREG_MCNT = 10
 LREG_TPTR = 11
 LREG_ENA = 12
-MAX_LREG = LREG_ENA
-
 LREG_POST = 13
+MAX_LREG_AI84 = LREG_ENA
 MAX_LREG_AI85 = LREG_POST
+MAX_LREG = MAX_LREG_AI84
 
-# Implementation specifics
-BIAS_DIV = 1
+BIAS_DIV_AI84 = 1
+BIAS_DIV_AI85 = 128
+BIAS_DIV = BIAS_DIV_AI84
 
 
 def set_device(ai85):
@@ -70,11 +85,19 @@ def set_device(ai85):
     global BIAS_DIV  # pylint: disable=global-statement
     global MAX_LREG  # pylint: disable=global-statement
     global MCNT_SAD_OFFS  # pylint: disable=global-statement
+    global C_BRAM_BASE  # pylint: disable=global-statement
+    global C_MRAM_BASE  # pylint: disable=global-statement
+    global C_SRAM_BASE  # pylint: disable=global-statement
+    global C_GROUP_OFFS  # pylint: disable=global-statement
 
     print(f'Configuring device: {"AI85" if ai85 else "AI84"}.')
     if not ai85:
         return
 
-    BIAS_DIV = 128
-    MCNT_SAD_OFFS = 16
+    BIAS_DIV = BIAS_DIV_AI85
+    MCNT_SAD_OFFS = MCNT_SAD_OFFS_AI85
     MAX_LREG = MAX_LREG_AI85
+    C_BRAM_BASE = C_BRAM_BASE_AI85
+    C_MRAM_BASE = C_MRAM_BASE_AI85
+    C_SRAM_BASE = C_SRAM_BASE_AI85
+    C_GROUP_OFFS = C_GROUP_OFFS_AI85
