bd06e14ad01a50d0422bd98fd0162e6bea560d1e
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-19 10:42:54 -0500

    Add small network (1 uJ inference) and extra small (just in case)

--- ai84net.py
+++ ai84net.py
@@ -6,7 +6,7 @@
 #
 ###################################################################################################
 """
-Network(s) that fit into AI84
+Networks that fit into AI84
 
 Optionally quantize/clamp activations
 """
@@ -73,7 +73,117 @@ class AI84Net5(nn.Module):
 
 def ai84net5(pretrained=False, **kwargs):
     """
-    Constructs a AI84Net-5 model.
+    Constructs a AI84Net5 model.
     """
     assert not pretrained
     return AI84Net5(**kwargs)
+
+
+class AI84NetExtraSmall(nn.Module):
+    """
+    Minimal CNN that tries to achieve 1uJ per inference for MNIST
+    """
+    def __init__(self, num_classes=10, num_channels=3, dimensions=(28, 28),
+                 simulate=False, fc_inputs=8, bias=False):
+        super(AI84NetExtraSmall, self).__init__()
+
+        # AI84 Limits
+        assert dimensions[0] == dimensions[1]  # Only square supported
+
+        # Keep track of image dimensions so one constructor works for all image sizes
+        dim = dimensions[0]
+
+        self.conv1 = ai84.FusedConv2dReLU(num_channels, 8, 3,
+                                          padding=1, bias=bias, simulate=simulate)
+        # padding 1 -> no change in dimensions -> 8x28x28
+
+        pad = 2 if dim == 28 else 1
+        self.conv2 = ai84.FusedMaxPoolConv2dReLU(8, 8, 3, pool_size=2, pool_stride=2,
+                                                 padding=pad, bias=bias, simulate=simulate)
+        dim //= 2  # pooling, padding 0 -> 8x14x14
+        if pad == 2:
+            dim += 2  # padding 2 -> 8x16x16
+
+        self.conv3 = ai84.FusedMaxPoolConv2dReLU(8, fc_inputs, 3,
+                                                 pool_size=4, pool_stride=4, padding=1,
+                                                 bias=bias, simulate=simulate)
+        dim //= 4  # pooling, padding 0 -> 8x4x4
+        # padding 1 -> 8x4x4
+
+        self.fc = ai84.SoftwareLinear(fc_inputs*dim*dim, num_classes, bias=True, simulate=simulate)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def ai84netextrasmall(pretrained=False, **kwargs):
+    """
+    Constructs a AI84NetExtraSmall model.
+    """
+    assert not pretrained
+    return AI84NetExtraSmall(**kwargs)
+
+
+class AI84NetSmall(nn.Module):
+    """
+    Minimal CNN that tries to achieve 1uJ per inference for MNIST
+    """
+    def __init__(self, num_classes=10, num_channels=3, dimensions=(28, 28),
+                 simulate=False, fc_inputs=12, bias=False):
+        super(AI84NetSmall, self).__init__()
+
+        # AI84 Limits
+        assert dimensions[0] == dimensions[1]  # Only square supported
+
+        # Keep track of image dimensions so one constructor works for all image sizes
+        dim = dimensions[0]
+
+        self.conv1 = ai84.FusedConv2dReLU(num_channels, 16, 3,
+                                          padding=1, bias=bias, simulate=simulate)
+        # padding 1 -> no change in dimensions -> 16x28x28
+
+        pad = 2 if dim == 28 else 1
+        self.conv2 = ai84.FusedMaxPoolConv2dReLU(16, 16, 3, pool_size=2, pool_stride=2,
+                                                 padding=pad, bias=bias, simulate=simulate)
+        dim //= 2  # pooling, padding 0 -> 16x14x14
+        if pad == 2:
+            dim += 2  # padding 2 -> 16x16x16
+
+        self.conv3 = ai84.FusedMaxPoolConv2dReLU(16, fc_inputs, 3,
+                                                 pool_size=4, pool_stride=4, padding=1,
+                                                 bias=bias, simulate=simulate)
+        dim //= 4  # pooling, padding 0 -> 16x4x4
+        # padding 1 -> 12x4x4
+
+        self.fc = ai84.SoftwareLinear(fc_inputs*dim*dim, num_classes, bias=True, simulate=simulate)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def ai84netsmall(pretrained=False, **kwargs):
+    """
+    Constructs a AI84NetSmall model.
+    """
+    assert not pretrained
+    return AI84NetSmall(**kwargs)
