3119a78b8585ec7c5e342b55fd161403c04a81e6
Robert Muchsel <robert.muchsel@maximintegrated.com>
2019-06-23 20:59:10 -0500

    Split project

--- evaluate_fashionmnist.sh
+++ evaluate_fashionmnist.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./train.py --model ai84net5 --dataset FashionMNIST --confusion --evaluate --ai84 --exp-load-weights-from trained/ai84-fashionmnist.pth.tar -8
+./train.py --model ai84net5 --dataset FashionMNIST --confusion --evaluate --ai84 --exp-load-weights-from ../ai8x-synthesis/trained/ai84-fashionmnist.pth.tar -8
