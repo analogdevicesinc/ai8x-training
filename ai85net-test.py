1084e5966e5966cdc7b99899b6f8134cd423c12b
Erman Okman <Erman.Okman@maximintegrated.com>
2019-11-01 18:22:50 -0500

    implementation of Fire module and SqueezeNet network is added. Train script of SquuezeNet for CIFAR10 dataset is included. Changes made to use updates in distiller repo.
    Change-Id: I7fe101c40fd73b934993c12039de83e9f1c3aa4a

--- ai85net-test.py
+++ ai85net-test.py
@@ -207,3 +207,90 @@ def ai85net6(pretrained=False, **kwargs):
     """
     assert not pretrained
     return AI85Net6(**kwargs)
+
+
+class AI85SqueezeNet(nn.Module):
+    """
+    SqueezeNet for AI85.
+    The last layer is implemented as a linear layer rather than a convolution
+    layer as defined in th eoriginal paper.
+    """
+    def __init__(self, num_channels=3, num_classes=10, dimensions=(32, 32),
+                 simulate=False, bias=False):
+        super(AI85SqueezeNet, self).__init__()
+        dim1 = dimensions[0]
+        dim2 = dimensions[1]
+        # 3x32x32
+        self.conv1 = ai84.FusedMaxPoolConv2dReLU(in_channels=num_channels, out_channels=64,
+                                                 kernel_size=3, padding=1, device=85,
+                                                 bias=bias, simulate=simulate)
+        dim1 //= 2
+        dim2 //= 2
+        # 64x16x16
+        self.fire1 = ai85.Fire(in_planes=64, squeeze_planes=16, expand1x1_planes=64,
+                               expand3x3_planes=64, bias=bias, simulate=simulate)
+        # 128x16x16
+        self.fire2 = ai85.Fire(in_planes=128, squeeze_planes=16, expand1x1_planes=64,
+                               expand3x3_planes=64, bias=bias, simulate=simulate)
+        # 128x16x16
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # check if kernel size=3
+        dim1 //= 2
+        dim2 //= 2
+        # 128x8x8
+        self.fire3 = ai85.Fire(in_planes=128, squeeze_planes=32, expand1x1_planes=128,
+                               expand3x3_planes=128, bias=bias, simulate=simulate)
+        # 256x8x8
+        self.fire4 = ai85.Fire(in_planes=256, squeeze_planes=32, expand1x1_planes=128,
+                               expand3x3_planes=128, bias=bias, simulate=simulate)
+        # 256x8x8
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)  # check if kernel size=3
+        dim1 //= 2
+        dim2 //= 2
+        # 256x4x4
+        self.fire5 = ai85.Fire(in_planes=256, squeeze_planes=48, expand1x1_planes=192,
+                               expand3x3_planes=192, bias=bias, simulate=simulate)
+        # 384x4x4
+        self.fire6 = ai85.Fire(in_planes=384, squeeze_planes=48, expand1x1_planes=192,
+                               expand3x3_planes=192, bias=bias, simulate=simulate)
+        # 384x4x4
+        self.fire7 = ai85.Fire(in_planes=384, squeeze_planes=48, expand1x1_planes=256,
+                               expand3x3_planes=256, bias=bias, simulate=simulate)
+        # 512x4x4
+        self.fire8 = ai85.Fire(in_planes=512, squeeze_planes=64, expand1x1_planes=256,
+                               expand3x3_planes=256, bias=bias, simulate=simulate)
+        # 512x4x4
+        # self.conv2 = ai84.FusedAvgPoolConv2dReLU(in_channels=512, out_channels=num_classes,
+        #                                          kernel_size=1, pool_size=4, pool_stride=4,
+        #                                          device=85)
+        self.fc = ai84.SoftwareLinear(512*dim1*dim2, num_classes, bias=bias, simulate=simulate)
+        # num_classesx1x1
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        x = self.conv1(x)
+        x = self.fire1(x)
+        x = self.fire2(x)
+        x = self.pool1(x)
+        x = self.fire3(x)
+        x = self.fire4(x)
+        x = self.pool2(x)
+        x = self.fire5(x)
+        x = self.fire6(x)
+        x = self.fire7(x)
+        x = self.fire8(x)
+        # x = self.conv2(x)
+        # x = x.view(x.size(0), -1)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+def ai85squeezenet(pretrained=False, **kwargs):
+    """
+    Constructs a AI85SqueezeNet model.
+    """
+    assert not pretrained
+    return AI85SqueezeNet(**kwargs)
