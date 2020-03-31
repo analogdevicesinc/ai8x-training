1eaa2928797d885053dea9e10a555805594f09e7
Afshin Niktash <Afshin.Niktash@maximintegrated.com>
2020-03-30 21:13:14 -0700

    support for 20 keywords in speechcom AI84
    Change-Id: I7f11ea78cee991a89e54976a33b03a5b08385132

--- ds_speechcomfolded1D.py
+++ ds_speechcomfolded1D.py
@@ -113,8 +113,8 @@ def speechcomfolded1D_get_datasets(data, load_train=True, load_test=True, num_cl
 
     The dataset is loaded from the archive file, so the file is required for this version.
 
-    The dataset originally includes 30 keywords. A dataset is formed with 7 classes which includes
-    6 of the original keywords ('up', 'down', 'left', 'right', 'stop', 'go') and the rest of the
+    The dataset originally includes 30 keywords. A dataset is formed with 7 (or 21) classes which includes
+    6 (or 20) of the original keywords and the rest of the
     dataset is used to form the last class, i.e class of the others.
     The dataset is split into training, validation and test sets. 80:10:10 training:validation:test
     split is used by default.
@@ -130,7 +130,7 @@ def speechcomfolded1D_get_datasets(data, load_train=True, load_test=True, num_cl
     ])
 
     if num_classes == 6:
-        classes = ['up', 'down', 'left', 'right', 'stop', 'go']  # 6 keywords
+        classes = ['up', 'down', 'left', 'right', 'stop', 'go']
     elif num_classes == 20:
         classes = ['up', 'down', 'left', 'right', 'stop', 'go', 'yes', 'no', 'on', 'off', 'one',
                    'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
