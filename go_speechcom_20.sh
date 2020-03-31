1361ae2457c5fa82f1254cf91167c088eaca2046
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-03-31 10:56:43 -0500

    Move KWS20 network to separate file

--- go_speechcom_20.sh
+++ go_speechcom_20.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress schedule.yaml --model ai84net20 --dataset SpeechCom_20 --confusion
+./train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress schedule.yaml --model ai85net20 --dataset SpeechCom_20 --confusion --device 85
