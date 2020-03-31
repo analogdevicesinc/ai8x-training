9d75cf3e216c471c4f2d3be9c80cfc6ff5357842
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-03-31 10:56:43 -0500

    Move KWS20 network to separate file

---
+++ ai85net-kws.py
@@ -0,0 +1,81 @@
+###################################################################################################
+#
+# Copyright (C) 2020 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+KWS Networks for AI85
+
+Optionally quantize/clamp activations
+"""
+import torch.nn as nn
+import ai8x
+
+
+class AI85Net20(nn.Module):
+    """
+    CNN that tries to achieve accuracy > %90 for kws.
+    """
+    def __init__(self, num_classes=21, num_channels=1, dimensions=(64, 64),
+                 fc_inputs=30, bias=False):
+        super(AI85Net20, self).__init__()
+
+        # AI84 Limits
+        assert dimensions[0] == dimensions[1]  # Only square supported
+
+        # Keep track of image dimensions so one constructor works for all image sizes
+        dim = dimensions[0]
+
+        self.conv1 = ai8x.FusedConv2dReLU(num_channels, 15, 3,
+                                          padding=1, bias=bias)
+        # padding 1 -> no change in dimensions -> 15x28x28
+
+        pad = 2 if dim == 28 else 1
+        self.conv2 = ai8x.FusedMaxPoolConv2dReLU(15, 30, 3, pool_size=2, pool_stride=2,
+                                                 padding=pad, bias=bias)
+        dim //= 2  # pooling, padding 0 -> 30x14x14
+        if pad == 2:
+            dim += 2  # padding 2 -> 30x16x16
+
+        self.conv3 = ai8x.FusedMaxPoolConv2dReLU(30, 60, 3, pool_size=2, pool_stride=2, padding=1,
+                                                 bias=bias)
+        dim //= 2  # pooling, padding 0 -> 60x8x8
+
+        self.conv4 = ai8x.FusedMaxPoolConv2dReLU(60, 30, 3, pool_size=2, pool_stride=2, padding=1,
+                                                 bias=bias)
+        dim //= 2  # pooling, padding 0 -> 30x4x4
+
+        self.conv5 = ai8x.FusedMaxPoolConv2dReLU(30, 30, 3, pool_size=2, pool_stride=2, padding=1,
+                                                 bias=bias)
+        dim //= 2  # pooling, padding 0 -> 30x2x2
+
+        self.conv6 = ai8x.FusedConv2dReLU(30, fc_inputs, 3, padding=1, bias=bias)
+
+        self.fc = ai8x.SoftwareLinear(fc_inputs*dim*dim, num_classes, bias=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def ai85net20(pretrained=False, **kwargs):
+    """
+    Constructs a AI84Net20 model.
+    """
+    assert not pretrained
+    return AI85Net20(**kwargs)
