2cc697637a5943f5e246783dab038d7800ef74cb
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-04-17 11:24:07 -0500

    Update KWS20 model

--- models/ai85net-audio.py
+++ models/ai85net-audio.py
@@ -20,15 +20,20 @@ class AI85AudioNet(nn.Module):
     Compound Audio Net, starting with Conv1Ds with kernel_size=1 and then switching to Conv2Ds
     """
     # num_classes = n keywords + 1 unknown
-    def __init__(self, num_classes=7, num_channels=512, dimensions=(64, 1),
-                 fc_inputs=10, bias=False):
+    def __init__(
+            self,
+            num_classes=21,
+            num_channels=128,
+            dimensions=(128, 1),  # pylint: disable=unused-argument
+            fc_inputs=7,
+            bias=False
+    ):
         super(AI85AudioNet, self).__init__()
 
-        dim1 = dimensions[0]
-        self.mfcc_conv1 = ai8x.FusedConv1dReLU(num_channels, 200, 1, stride=1, padding=0,
+        self.mfcc_conv1 = ai8x.FusedConv1dReLU(num_channels, 100, 1, stride=1, padding=0,
                                                bias=bias)
 
-        self.mfcc_conv2 = ai8x.FusedConv1dReLU(200, 50, 1, stride=1, padding=0,
+        self.mfcc_conv2 = ai8x.FusedConv1dReLU(100, 50, 1, stride=1, padding=0,
                                                bias=bias)
 
         self.mfcc_conv3 = ai8x.FusedConv1dReLU(50, 50, 1, stride=1, padding=0,
@@ -43,44 +48,43 @@ class AI85AudioNet(nn.Module):
         self.mfcc_conv6 = ai8x.FusedConv1dReLU(50, 16, 1, stride=1, padding=0,
                                                bias=bias)
 
-        dim2 = 16
-        self.kws_conv1 = ai8x.FusedConv2dReLU(1, 15, 3, stride=1, padding=1,
+        self.kws_conv1 = ai8x.FusedConv2dReLU(16, 15, 3, stride=1, padding=1,
                                               bias=bias)
 
-        self.kws_conv2 = ai8x.FusedConv2dReLU(15, 30, 3, padding=1,
+        self.kws_conv2 = ai8x.FusedConv2dReLU(15, 30, 3, stride=1, padding=1,
                                               bias=bias)
 
-        self.kws_conv3 = ai8x.FusedMaxPoolConv2dReLU(30, 60, 3, pool_size=2, pool_stride=2,
-                                                     padding=1, bias=bias)
-        dim1 //= 2
-        dim2 //= 2
+        self.kws_conv3 = ai8x.FusedConv2dReLU(30, 60, 3, stride=1,
+                                              padding=1, bias=bias)
 
-        self.kws_conv4 = ai8x.FusedMaxPoolConv2dReLU(60, 30, 3, pool_size=2, pool_stride=2,
-                                                     padding=1, bias=bias)
-        dim1 //= 2
-        dim2 //= 2
+        self.kws_conv4 = ai8x.FusedConv2dReLU(60, 30, 3, stride=1,
+                                              padding=1, bias=bias)
 
         self.kws_conv5 = ai8x.FusedConv2dReLU(30, 30, 3, padding=1, bias=bias)
 
-        self.kws_conv6 = ai8x.FusedMaxPoolConv2dReLU(30, fc_inputs, 3, pool_size=2, pool_stride=2,
-                                                     padding=1, bias=bias)
-        dim1 //= 2
-        dim2 //= 2
+        self.kws_conv6 = ai8x.FusedConv2dReLU(30, fc_inputs, 3, stride=1,
+                                              padding=1, bias=bias)
 
-        self.fc = ai8x.SoftwareLinear(fc_inputs*dim1*dim2, num_classes, bias=bias)
+        self.fc = ai8x.Linear(fc_inputs*128, num_classes, bias=bias)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def forward(self, x):  # pylint: disable=arguments-differ
+        # Save input sample for CNN generation
+        # import numpy as np
+        # a = x.cpu().numpy().squeeze().astype('int64')
+        # a = np.clip(a, -128, 127)
+        # np.save('sample_speechcomfolded1d_20', a, allow_pickle=False, fix_imports=False)
+        # Run CNN
         x = self.mfcc_conv1(x)
         x = self.mfcc_conv2(x)
         x = self.mfcc_conv3(x)
         x = self.mfcc_conv4(x)
         x = self.mfcc_conv5(x)
         x = self.mfcc_conv6(x)
-        x = x.view(x.shape[0], 1, x.shape[1], x.shape[2])
+        x = x.view(x.shape[0], x.shape[1], 16, -1)
         x = self.kws_conv1(x)
         x = self.kws_conv2(x)
         x = self.kws_conv3(x)
