51b09db9eaf3e6bed480258e8a5a6bf52360519c
Erman Okman <Erman.Okman@maximintegrated.com>
2020-05-08 12:03:05 +0300

    cifar100 dataloader and cifar100 train script is added
    Change-Id: I152da565706d9ad1b4883932d6ea441b731f6404

---
+++ datasets/cifar100.py
@@ -0,0 +1,108 @@
+###################################################################################################
+#
+# Copyright (C) 2018-2020 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+#
+# Portions Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+CIFAR-100 Dataset
+"""
+import os
+
+import torchvision
+from torchvision import transforms
+
+import ai8x
+
+
+def cifar100_get_datasets(data, load_train=True, load_test=True):
+    """
+    Load the CIFAR100 dataset.
+
+    The original training dataset is split into training and validation sets (code is
+    inspired by https://gist.github.com/kevinzakka/d33bf8d6c7f06a9d8c76d97a7879f5cb).
+    By default we use a 90:10 (45K:5K) training:validation split.
+
+    The output of torchvision datasets are PIL Image images of range [0, 1].
+    We transform them to Tensors of normalized range [-1, 1]
+    https://github.com/pytorch/tutorials/blob/master/beginner_source/blitz/cifar10_tutorial.py
+
+    Data augmentation: 4 pixels are padded on each side, and a 32x32 crop is randomly sampled
+    from the padded image or its horizontal flip.
+    This is similar to [1] and some other work that use CIFAR10.
+
+    [1] C.-Y. Lee, S. Xie, P. Gallagher, Z. Zhang, and Z. Tu. Deeply Supervised Nets.
+    arXiv:1409.5185, 2014
+    """
+    (data_dir, args) = data
+
+    if load_train:
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            ai8x.normalize(args=args)
+        ])
+
+        train_dataset = torchvision.datasets.CIFAR100(root=os.path.join(data_dir, 'CIFAR100'),
+                                                      train=True, download=True,
+                                                      transform=train_transform)
+    else:
+        train_dataset = None
+
+    if load_test:
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            ai8x.normalize(args=args)
+        ])
+
+        test_dataset = torchvision.datasets.CIFAR100(root=os.path.join(data_dir, 'CIFAR100'),
+                                                     train=False, download=True,
+                                                     transform=test_transform)
+
+        if args.truncate_testset:
+            test_dataset.data = test_dataset.data[:1]
+    else:
+        test_dataset = None
+
+    return train_dataset, test_dataset
+
+
+datasets = [
+    {
+        'name': 'CIFAR100',
+        'input': (3, 32, 32),
+        'output': ('apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle',
+                   'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel',
+                   'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock',
+                   'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 
+                   'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 
+                   'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 
+                   'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 
+                   'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 
+                   'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 
+                   'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 
+                   'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 
+                   'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 
+                   'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 
+                   'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm'),
+        'loader': cifar100_get_datasets,
+    },
+]
