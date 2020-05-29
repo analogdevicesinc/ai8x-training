968260b9979db16c3db1a3a87c77d9e768898c04
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-05-29 17:17:37 -0500

    Forklift to PyTorch 1.5 using Maxim's Distiller branch
    Remove TensorFlow requirement

--- train.py
+++ train.py
@@ -1189,12 +1189,12 @@ def save_collectors_data(collectors, directory):
 
 
 def check_pytorch_version():
-    """Ensure PyTorch >= 1.3.1"""
-    if parse_version(torch.__version__) < parse_version('1.3.1'):
+    """Ensure PyTorch >= 1.5.0"""
+    if parse_version(torch.__version__) < parse_version('1.5.0'):
         print("\nNOTICE:")
-        print("The Distiller \'master\' branch now requires at least PyTorch version 1.3.1 due to "
+        print("This software requires at least PyTorch version 1.5.0 due to "
               "PyTorch API changes which are not backward-compatible.\n"
-              "Please install PyTorch 1.3.1 or its derivative.\n"
+              "Please install PyTorch 1.5.0 or its derivative.\n"
               "If you are using a virtual environment, do not forget to update it:\n"
               "  1. Deactivate the old environment\n"
               "  2. Install the new environment\n"
