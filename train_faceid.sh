868c4829796cfced4876c97c834778f8eb79d253
Erman Okman <Erman.Okman@maximintegrated.com>
2020-06-03 03:49:58 +0300

    Fixes train_faceid.sh

--- train_faceid.sh
+++ train_faceid.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress schedule-faceid.yaml --model ai85faceidnet --dataset FaceID --batch-size 100 --device 85 --regression --print-freq 250
+./train.py --epochs 100 --optimizer Adam --lr 0.001 --deterministic --compress schedule-faceid.yaml --model ai85faceidnet --dataset FaceID --batch-size 100 --device 85 --regression --print-freq 250  $@
