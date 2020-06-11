a86a5a36c9441506b2962a9e6a79416ec9b02325
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-06-11 14:43:28 -0500

    Add MNIST 'extra small' model (#17)

--- models/ai85net.py
+++ models/ai85net.py
@@ -79,9 +79,69 @@ def ai85net5(pretrained=False, **kwargs):
     return AI85Net5(**kwargs)
 
 
+class AI85NetExtraSmall(nn.Module):
+    """
+    Minimal CNN for minimum energy per inference for MNIST
+    """
+    def __init__(self, num_classes=10, num_channels=3, dimensions=(28, 28),
+                 fc_inputs=8, bias=False):
+        super(AI85NetExtraSmall, self).__init__()
+
+        # AI84 Limits
+        assert dimensions[0] == dimensions[1]  # Only square supported
+
+        # Keep track of image dimensions so one constructor works for all image sizes
+        dim = dimensions[0]
+
+        self.conv1 = ai8x.FusedConv2dReLU(num_channels, 8, 3,
+                                          padding=1, bias=bias)
+        # padding 1 -> no change in dimensions -> 8x28x28
+
+        pad = 2 if dim == 28 else 1
+        self.conv2 = ai8x.FusedMaxPoolConv2dReLU(8, 8, 3, pool_size=2, pool_stride=2,
+                                                 padding=pad, bias=bias)
+        dim //= 2  # pooling, padding 0 -> 8x14x14
+        if pad == 2:
+            dim += 2  # padding 2 -> 8x16x16
+
+        self.conv3 = ai8x.FusedMaxPoolConv2dReLU(8, fc_inputs, 3,
+                                                 pool_size=4, pool_stride=4, padding=1,
+                                                 bias=bias)
+        dim //= 4  # pooling, padding 0 -> 8x4x4
+        # padding 1 -> 8x4x4
+
+        self.fc = ai8x.Linear(fc_inputs*dim*dim, num_classes, bias=True, wide=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def ai85netextrasmall(pretrained=False, **kwargs):
+    """
+    Constructs a AI85NetExtraSmall model.
+    """
+    assert not pretrained
+    return AI85NetExtraSmall(**kwargs)
+
+
 models = [
     {
         'name': 'ai85net5',
+        'min_input': 1,
+        'dim': 2,
+    },
+    {
+        'name': 'ai85netextrasmall',
         'min_input': 1,
         'dim': 2,
     },
