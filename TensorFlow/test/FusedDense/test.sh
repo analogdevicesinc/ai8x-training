0ad911d32306e1a40b2cd6abebee623cf0abf46b
Afshin Niktash <Afshin.Niktash@maximintegrated.com>
2020-08-28 17:16:21 -0500

    run all tests and extract final outputs in test_result.log

--- TensorFlow/test/FusedDense/test.sh
+++ TensorFlow/test/FusedDense/test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 python test.py
-python -m tf2onnx.convert --saved-model saved_model --opset 10 --output saved_model/saved_model.onnx
-
+python -m tf2onnx.convert --saved-model saved_model --inputs-as-nchw input_1:0 --opset 10 --output saved_model/saved_model.onnx
