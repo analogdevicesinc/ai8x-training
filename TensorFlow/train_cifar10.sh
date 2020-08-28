c9412f678e3dd853c0be734c4d7f8a6b2cb7e148
Victor Loginov <Victor.Loginov@maximintegrated.com>
2020-08-27 20:27:59 -0500

    added nchw option to ONNX converter

--- TensorFlow/train_cifar10.sh
+++ TensorFlow/train_cifar10.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python train.py --epochs 100 --batch_size 64 --optimizer Adam --lr 0.0003 --model cifar10_model --dataset cifar10 --save-sample 1431 --save-sample-per-class $@
-python -m tf2onnx.convert --saved-model export/cifar10 --opset 10 --output export/cifar10/saved_model.onnx
+python -m tf2onnx.convert --saved-model export/cifar10 --inputs-as-nchw input_1:0 --opset 10 --output export/cifar10/saved_model.onnx
+
--- TensorFlow/train_mnist.sh
+++ TensorFlow/train_mnist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python train.py --epochs 100 --batch_size 256 --optimizer Adam --lr 0.001 --model mnist_model --dataset mnist --save-sample 1 --save-sample-per-class $@
-python -m tf2onnx.convert --saved-model export/mnist --opset 10 --output export/mnist/saved_model.onnx
+python -m tf2onnx.convert --saved-model export/mnist --inputs-as-nchw input_1:0 --opset 10 --output export/mnist/saved_model.onnx
