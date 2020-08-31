1445878937d10de590184a6695ce2b2be4be40e5
Afshin Niktash <Afshin.Niktash@maximintegrated.com>
2020-08-31 15:48:56 -0500

    added flag for nchw in gennerated onnx for evaluate

--- TensorFlow/evaluate_kws20.sh
+++ TensorFlow/evaluate_kws20.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python evaluate.py --onnx-file export/kws20/saved_model.onnx --dataset kws20
-python evaluate.py --onnx-file export/kws20/saved_model_quant.onnx --dataset kws20
+python evaluate.py --onnx-file export/kws20/saved_model_dq.onnx --dataset kws20
