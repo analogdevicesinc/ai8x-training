48f69cd8751b45124e1e367f661fadfc0c87b095
Afshin Niktash <Afshin.Niktash@maximintegrated.com>
2020-09-08 16:46:53 -0700

    removed test folder and unnecessary files from this branch

--- TensorFlow/test/Conv2D_Conv2D_Dense/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python test.py
-python -m tf2onnx.convert --saved-model saved_model --inputs-as-nchw input_1:0 --opset 10 --output saved_model/saved_model.onnx
--- TensorFlow/test/FusedAvgPoolConv2DReLU_bias/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python test.py
-python -m tf2onnx.convert --saved-model saved_model --opset 10 --output saved_model/saved_model.onnx
