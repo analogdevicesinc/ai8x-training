dfa9d324d646cc72e48c77ced93c24140b16341f
Afshin Niktash <Afshin.Niktash@maximintegrated.com>
2020-10-19 11:59:53 -0700

    linting cleanup

--- TensorFlow/test/SimpleNet3/test.py
+++ TensorFlow/test/SimpleNet3/test.py
@@ -102,7 +102,7 @@ conv1 = ai8xTF.FusedConv2DTranspose(
     filters=1,
     kernel_size=3,
     strides=2,
-    #padding_size=1,
+    # padding_size=1,
     use_bias=False,
     kernel_initializer=tf.keras.initializers.constant(k1)
     )(reshape)
