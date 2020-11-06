64921f8cd150a4f33aeee08d9494c0fc150d2ce2
Robert Muchsel <robert.muchsel@maximintegrated.com>
2020-11-06 14:18:27 -0600

    Move individual training scripts to scripts/

--- train_all_models.sh
+++ train_all_models.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 echo "-----------------------------"
 echo "Training cats&dogs model"
-./train_catsdogs.sh
+scripts/train_catsdogs.sh
 echo "-----------------------------"
 echo "Training mnist model"
-./train_mnist.sh
+scripts/train_mnist.sh
 echo "-----------------------------"
 echo "Training mnist_extrasmall model"
-./train_mnist_extrasmall.sh
+scripts/train_mnist_extrasmall.sh
 echo "-----------------------------"
 echo "Training cifar10 model"
-./train_cifar10.sh
+scripts/train_cifar10.sh
 echo "-----------------------------"
 echo "Training cifar100 model without QAT"
-./train_cifar100.sh
+scripts/train_cifar100.sh
 echo "-----------------------------"
 echo "Training cifar100 model with 8-bit QAT"
-./train_cifar100_qat8.sh
+scripts/train_cifar100_qat8.sh
 echo "-----------------------------"
 echo "Training cifar100 model with mixed bit width QAT"
-./train_cifar100_qat_mixed.sh
+scripts/train_cifar100_qat_mixed.sh
 echo "-----------------------------"
 echo "Training cifar100_residual model"
-./train_cifar100_ressimplenet.sh
+scripts/train_cifar100_ressimplenet.sh
 echo "-----------------------------"
 echo "Training kws20 model"
-./train_kws20.sh
+scripts/train_kws20.sh
 echo "-----------------------------"
 echo "Training kws20_v2 model"
-./train_kws20_v2.sh
+scripts/train_kws20_v2.sh
 echo "-----------------------------"
 echo "Training faceid model"
-./train_faceid.sh
+scripts/train_faceid.sh
 
