236c5f13962079b1f11f014f3cb892a439c59745
Batuhan Gundogdu-MacPro <m.batuhangundogdu@gmail.com>
2020-12-16 01:51:26 +0300

    update v3

--- models/ai85net-kws20-v3.py
+++ models/ai85net-kws20-v3.py
@@ -10,14 +10,13 @@
 Keyword spotting network for AI85/AI86
 """
 import torch.nn as nn
-import ai8x
-import torch
 
+import ai8x
 
 
 class AI85KWS20Netv3(nn.Module):
     """
-    E2E KWS with all Conv-1Ds
+    Compound KWS20 v3 Audio net, all with Conv1Ds
     """
 
     # num_classes = n keywords + 1 unknown
@@ -26,42 +25,31 @@ class AI85KWS20Netv3(nn.Module):
             num_classes=21,
             num_channels=128,
             dimensions=(128, 1),  # pylint: disable=unused-argument
-            fc_inputs=7,
-            activation_k=1,
-            activation_2=1,
-            bias=False
+            bias=False,
+			**kwargs
 
     ):
         super().__init__()
 
         self.drop =  nn.Dropout(p=0.2)
-############# T: 128 F :128
-        self.voice_conv1 = ai8x.FusedConv1dReLU(num_channels, 100, 1, stride=1, padding=0,activation_k=activation_k,
-                                                bias=bias)
-############ T:  128 F: 100
-        self.voice_conv2 = ai8x.FusedConv1dReLU(100, 96, 3, stride=1, padding=0,activation_k=activation_k,
-                                                bias=bias)
-###########  T: 126 F : 96
-        self.voice_conv3 = ai8x.FusedMaxPoolConv1dReLU(96, 64, 3, stride=1, padding=1,activation_k=activation_k,
-                                                bias=bias)
-##########   T: 62 F : 64
-        self.voice_conv4 = ai8x.FusedConv1dReLU(64, 48, 3, stride=1, padding=0,activation_k=activation_k,
-                                                bias=bias)
-##########  T : 60 F : 48
-        self.kws_conv1 = ai8x.FusedMaxPoolConv1dReLU(48, 64, 3, stride=1, padding=1,activation_k=activation_2,
-                                              bias=bias)
-#########   T: 30 F : 64
-        self.kws_conv2 = ai8x.FusedConv1dReLU(64, 96, 3, stride=1, padding=0,activation_k=activation_2,
-                                              bias=bias)
-#########   T: 28 F : 96
-        self.kws_conv3 = ai8x.FusedAvgPoolConv1dReLU(96, 100, 3, stride=1, padding=1,activation_k=activation_2,
-                                              bias=bias)
-##########  T : 14 F: 100
-
-        self.kws_conv4 = ai8x.FusedMaxPoolConv1dReLU(100, 64, 6, stride=1, padding=1,activation_k=activation_2,
-                                              bias=bias)
-#########   T : 2 F: 128
-
+		
+        #Time: 128 Feature :128
+        self.voice_conv1 = ai8x.FusedConv1dReLU(num_channels, 100, 1, stride=1, padding=0, bias=bias,**kwargs)
+        # T:  128 F: 100
+        self.voice_conv2 = ai8x.FusedConv1dReLU(100, 96, 3, stride=1, padding=0, bias=bias, **kwargs)
+		# T: 126 F : 96
+        self.voice_conv3 = ai8x.FusedMaxPoolConv1dReLU(96, 64, 3, stride=1, padding=1, bias=bias, **kwargs)
+		# T: 62 F : 64
+        self.voice_conv4 = ai8x.FusedConv1dReLU(64, 48, 3, stride=1, padding=0, bias=bias, **kwargs)
+		# T : 60 F : 48
+        self.kws_conv1 = ai8x.FusedMaxPoolConv1dReLU(48, 64, 3, stride=1, padding=1, bias=bias, **kwargs)
+		# T: 30 F : 64
+        self.kws_conv2 = ai8x.FusedConv1dReLU(64, 96, 3, stride=1, padding=0, bias=bias, **kwargs)
+		# T: 28 F : 96
+        self.kws_conv3 = ai8x.FusedAvgPoolConv1dReLU(96, 100, 3, stride=1, padding=1, bias=bias, **kwargs)
+		# T : 14 F: 100
+        self.kws_conv4 = ai8x.FusedMaxPoolConv1dReLU(100, 64, 6, stride=1, padding=1, bias=bias, **kwargs)
+        # T : 2 F: 128
         self.fc = ai8x.Linear(256, num_classes, bias=bias)
         
 
