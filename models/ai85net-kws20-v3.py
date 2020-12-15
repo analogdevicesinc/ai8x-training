fef308fd03e93b8f291f3a7b4b2a477e807a30e1
Batuhan Gundogdu-MacPro <m.batuhangundogdu@gmail.com>
2020-12-16 02:00:12 +0300

    Update ai85net-kws20-v3.py

--- models/ai85net-kws20-v3.py
+++ models/ai85net-kws20-v3.py
@@ -32,28 +32,32 @@ class AI85KWS20Netv3(nn.Module):
         super().__init__()
 
         self.drop =  nn.Dropout(p=0.2)
-		
-        #Time: 128 Feature :128
-        self.voice_conv1 = ai8x.FusedConv1dReLU(num_channels, 100, 1, stride=1, padding=0, bias=bias,**kwargs)
+		#Time: 128 Feature :128
+        self.voice_conv1 = ai8x.FusedConv1dReLU(num_channels, 100, 1, stride=1, padding=0,
+												bias=bias,**kwargs)
         # T:  128 F: 100
-        self.voice_conv2 = ai8x.FusedConv1dReLU(100, 96, 3, stride=1, padding=0, bias=bias, **kwargs)
+        self.voice_conv2 = ai8x.FusedConv1dReLU(100, 96, 3, stride=1, padding=0,
+												bias=bias, **kwargs)
 		# T: 126 F : 96
-        self.voice_conv3 = ai8x.FusedMaxPoolConv1dReLU(96, 64, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.voice_conv3 = ai8x.FusedMaxPoolConv1dReLU(96, 64, 3, stride=1, padding=1,
+													   bias=bias, **kwargs)
 		# T: 62 F : 64
-        self.voice_conv4 = ai8x.FusedConv1dReLU(64, 48, 3, stride=1, padding=0, bias=bias, **kwargs)
+        self.voice_conv4 = ai8x.FusedConv1dReLU(64, 48, 3, stride=1, padding=0,
+												bias=bias, **kwargs)
 		# T : 60 F : 48
-        self.kws_conv1 = ai8x.FusedMaxPoolConv1dReLU(48, 64, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.kws_conv1 = ai8x.FusedMaxPoolConv1dReLU(48, 64, 3, stride=1, padding=1,
+													 bias=bias, **kwargs)
 		# T: 30 F : 64
-        self.kws_conv2 = ai8x.FusedConv1dReLU(64, 96, 3, stride=1, padding=0, bias=bias, **kwargs)
+        self.kws_conv2 = ai8x.FusedConv1dReLU(64, 96, 3, stride=1, padding=0,
+											  bias=bias, **kwargs)
 		# T: 28 F : 96
-        self.kws_conv3 = ai8x.FusedAvgPoolConv1dReLU(96, 100, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.kws_conv3 = ai8x.FusedAvgPoolConv1dReLU(96, 100, 3, stride=1, padding=1,
+													 bias=bias, **kwargs)
 		# T : 14 F: 100
-        self.kws_conv4 = ai8x.FusedMaxPoolConv1dReLU(100, 64, 6, stride=1, padding=1, bias=bias, **kwargs)
+        self.kws_conv4 = ai8x.FusedMaxPoolConv1dReLU(100, 64, 6, stride=1, padding=1,
+													 bias=bias, **kwargs)
         # T : 2 F: 128
         self.fc = ai8x.Linear(256, num_classes, bias=bias)
-        
-
-
 
     def forward(self, x):  # pylint: disable=arguments-differ
         # Run CNN
