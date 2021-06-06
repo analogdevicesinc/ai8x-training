043d79c37c20f894ac0d03a4644aecbf8d216c27
Erman Okman <Erman.Okman@maximintegrated.com>
2021-06-07 01:26:14 +0300

    modifies UNet training script

--- scripts/train_camvid_unet.sh
+++ scripts/train_camvid_unet.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./train.py --epochs 100 --optimizer Adam --lr 0.005 --model ai85unet_v3 --dataset CamVidAll --device MAX78000 --batch-size 100 --qat-policy qat_policy_camvid.yaml --use-bias --data /data/raw/CamVid_All/ --validation-split 0 "$@"
+./train.py --epochs 100 --optimizer Adam --lr 0.001 --model ai85unet_v3 --dataset CamVidAll --device MAX78000 --batch-size 100 --qat-policy qat_policy_camvid.yaml --use-bias --data /data/raw/CamVid_All/ --validation-split 0 "$@"
