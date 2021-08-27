cc31249cea10e233c96f85c3f2a5a1af6d177e4f
Robert Muchsel <robert.muchsel@maximintegrated.com>
2021-08-27 17:19:07 -0500

    Update train_asl.sh

--- scripts/train_asl.sh
+++ scripts/train_asl.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./train.py --epochs 100 --optimizer Adam --lr 0.00030 --batch-size 256 --deterministic --compress schedule-rps.yaml --model ai85rpsnet --dataset asl_big --confusion --device MAX78000 --use-bias "$@"
+python3 train.py --epochs 100 --optimizer Adam --lr 0.00030 --batch-size 256 --deterministic --compress schedule-rps.yaml --model ai85rpsnet --dataset asl_big --confusion --device MAX78000 --use-bias "$@"
