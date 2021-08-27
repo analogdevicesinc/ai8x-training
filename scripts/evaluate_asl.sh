acd4d6b484a8800363bdf8a825169a3bdfc0df02
Robert Muchsel <robert.muchsel@maximintegrated.com>
2021-08-27 17:15:38 -0500

    Update evaluate_asl.sh

--- scripts/evaluate_asl.sh
+++ scripts/evaluate_asl.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
 ./train.py --model ai85rpsnet --dataset asl_big --confusion --evaluate --exp-load-weights-from ../ai8x-synthesis/trained/ai85-asl01-chw.pth.tar -8 --device MAX78000 --use-bias "$@"
-
-#./train.py --model ai85nascifarnet --dataset asl_all --confusion --evaluate --exp-load-weights-from ./logs/2021.07.06-132039/checkpoint.pth.tar --device MAX78000 "$@"
