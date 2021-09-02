4ed29ddbc0a6115a83ae8a8fbf01041277cb9051
Jack Warren <Jack.Warren@maximintegrated.com>
2021-09-02 14:06:51 -0700

    new files added

---
+++ models/ai85net-asl.py
@@ -0,0 +1,94 @@
+###################################################################################################
+#
+# Copyright (C) Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+ASL classification network for AI85
+"""
+import torch.nn as nn
+
+import ai8x
+
+
+class AI85ASLNet(nn.Module):
+    """
+    Define CNN model for image classification.
+    """
+    def __init__(self, num_classes=27, num_channels=3, dimensions=(64, 64),
+                 fc_inputs=32, bias=False, **kwargs):
+        super().__init__()
+
+        # AI85 Limits
+        assert dimensions[0] == dimensions[1]  # Only square supported
+
+        # Keep track of image dimensions so one constructor works for all image sizes
+        dim = dimensions[0]
+
+        self.conv1 = ai8x.FusedConv2dReLU(num_channels, 16, 3,
+                                          padding=1, bias=bias, **kwargs)
+        # padding 1 -> no change in dimensions -> 16x64x64
+
+        pad = 2 if dim == 28 else 1
+        self.dropout1 = nn.Dropout(0.2)
+        self.conv2 = ai8x.FusedMaxPoolConv2dReLU(16, 32, 3, pool_size=2, pool_stride=2,
+                                                 padding=pad, bias=bias, **kwargs)
+        dim //= 2  # pooling, padding 0 -> 30x32x32
+        if pad == 2:
+            dim += 2  # padding 2 -> 30x16x16
+
+        self.conv3 = ai8x.FusedMaxPoolConv2dReLU(32, 60, 3, pool_size=2, pool_stride=2, padding=1,
+                                                 bias=bias, **kwargs)
+        dim //= 2  # pooling, padding 0 -> 60x16x16
+
+        self.conv4 = ai8x.FusedMaxPoolConv2dReLU(60, 32, 3, pool_size=2, pool_stride=2, padding=1,
+                                                 bias=bias, **kwargs)
+        dim //= 2  # pooling, padding 0 -> 30x8x8
+
+        self.conv5 = ai8x.FusedMaxPoolConv2dReLU(32, 32, 3, pool_size=2, pool_stride=2, padding=1,
+                                                 bias=bias, **kwargs)
+        dim //= 2  # pooling, padding 0 -> 30x4x4
+
+        self.conv6 = ai8x.FusedConv2dReLU(32, fc_inputs, 3, padding=1, bias=bias, **kwargs)
+
+        self.fc = ai8x.Linear(fc_inputs * dim * dim, num_classes, bias=True, **kwargs)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        """Forward prop"""
+        x = self.conv1(x)
+        x = self.dropout1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.dropout1(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.dropout1(x)
+        x = self.conv6(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def ai85aslnet(pretrained=False, **kwargs):
+    """
+    Constructs a AI85ASLNet model.
+    """
+    assert not pretrained
+    return AI85ASLNet(**kwargs)
+
+
+models = [
+    {
+        'name': 'ai85aslnet',
+        'min_input': 1,
+        'dim': 3,
+    },
+]
