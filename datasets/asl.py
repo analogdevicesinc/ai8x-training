1b20c82592e5f5dbf8286831faae77d257859e22
Jack Warren <Jack.Warren@maximintegrated.com>
2021-09-03 10:41:39 -0700

    fixing linter errors

--- datasets/asl.py
+++ datasets/asl.py
@@ -10,12 +10,10 @@
 ASL Datasets
 """
 import os
-import shutil
 import sys
-import torch
 import torchvision
-from torchvision import transforms
 import ai8x
+from torchvision import transforms
 
 def asl_get_datasets(data, load_train=True, load_test=True):
     """
@@ -25,42 +23,39 @@ def asl_get_datasets(data, load_train=True, load_test=True):
     path = data_dir
     dataset_path = os.path.join(path, "asl_big")
     is_dir = os.path.isdir(dataset_path)
-    
     if not is_dir:
-            print("******************************************")
-            print("Please follow instructions below:")
-            print("Download the dataset in the current working directory by visiting this link"
-                  "https://www.kaggle.com/grassknoted/asl-alphabet")
-            print("and click the \'Download (1 GB)\' button")
-            print("If you do not have a Kaggle account, sign-up first.")
-            print("Unzip 'asl_alphabet_test.zip' and  'asl_alphabet_test.zip.'"
-                  "Rename these folders to test and train respectivley"
-                  "and store in folder labeled 'asl_big'."
-                  "Delete space folder since it was not used." 
-                  "More files can be added from train to test folders, if needed.")
-            print("******************************************")
-            sys.exit("Dataset not found..")
-    
+        print("******************************************")
+        print("Please follow instructions below:")
+        print("Download the dataset in the current working directory by visiting this link"
+              "https://www.kaggle.com/grassknoted/asl-alphabet")
+        print("and click the \'Download (1 GB)\' button")
+        print("If you do not have a Kaggle account, sign-up first.")
+        print("Unzip 'asl_alphabet_test.zip' and  'asl_alphabet_test.zip.'"
+              "Rename these folders to test and train respectivley"
+              "and store in folder labeled 'asl_big'."
+              "Delete space folder since it was not used."
+              "More files can be added from train to test folders, if needed.")
+        print("******************************************")
+        sys.exit("Dataset not found..")
     training_data_path = os.path.join(data_dir, "asl_big")
     training_data_path = os.path.join(training_data_path, "train")
     test_data_path = os.path.join(data_dir, "asl_big")
     test_data_path = os.path.join(test_data_path, "test")
-    
     # Loading and normalizing train dataset
     if load_train:
         train_transform = transforms.Compose([
             transforms.Resize((64, 64)),
             transforms.ColorJitter(
-                brightness=(0.3, .8), #*torch.randn(0, 1),
-                contrast=(.7, 1), #*torch.rand(1),
-               saturation=0.2, #*torch.randn(0, 1),
-                ), #*torch.randn(0, 1)),
+                brightness=(0.3, .8),
+                contrast=(.7, 1),
+               saturation=0.2,
+                ),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply(([
                 transforms.ColorJitter(),
                 ]), p=0.3),
-            #transforms.RandomAffine(degrees=20, translate=(0.1, 0.1), shear=5),
-            #transforms.RandomGrayscale(p=0.2),
+            # transforms.RandomAffine(degrees=20, translate=(0.1, 0.1), shear=5),
+            # transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
             ai8x.normalize(args=args)
         ])
