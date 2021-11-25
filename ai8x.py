ea99f955c3965618846a764f5256a56e3e916872
Victor Loginov <victor.loginov@maximintegrated.com>
2021-11-24 19:35:38 -0600

    Cifar100 efficient net v2 example

--- ai8x.py
+++ ai8x.py
@@ -13,6 +13,7 @@ the limits into account.
 import torch
 import torch.nn as nn
 from torch.autograd import Function
+from torch.nn import functional as F
 
 import devices
 
@@ -548,6 +549,9 @@ class Conv2d(QuantizationAwareModule):
             weight_bits=None,
             bias_bits=None,
             quantize_activation=False,
+            groups=1,
+            eps=1e-05, 
+            momentum=0.05
     ):
         assert not wide or activation is None
 
@@ -606,10 +610,10 @@ class Conv2d(QuantizationAwareModule):
             pool = None
 
         if batchnorm == 'Affine':
-            bn = nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.05, affine=True)
+            bn = nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum, affine=True)
             assert bias, '`bias` must be set (enable --use-bias for models where bias is optional)'
         elif batchnorm == 'NoAffine':
-            bn = nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.05, affine=False)
+            bn = nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum, affine=False)
             assert bias, '`bias` must be set (enable --use-bias for models where bias is optional)'
         else:
             bn = None
@@ -624,7 +628,7 @@ class Conv2d(QuantizationAwareModule):
             if op == 'Conv2d':
                 opn = nn.Conv2d(in_channels, out_channels,
                                 kernel_size=kernel_size, stride=stride,
-                                padding=padding, dilation=dilation, bias=bias)
+                                padding=padding, dilation=dilation, bias=bias, groups=groups)
             elif op == 'ConvTranspose2d':
                 assert dev.device != 84
                 opn = nn.ConvTranspose2d(in_channels, out_channels,
@@ -1528,3 +1532,105 @@ def onnx_export_prep(m, simplify=False):
                 setattr(m, attr_str, ScalerONNX())
 
     m.apply(_onnx_export_prep)
+
+
+class MBConvBlock(nn.Module):
+    """Mobile Inverted Residual Bottleneck Block.
+
+    Args:
+        image_size (tuple or list): [image_height, image_width].
+        in_channels: number of input channels
+        out_channels: number of output channels
+        kernel_size: kernel size (default 3)
+        stride: stride size (default 1)
+        se_ratio: squeeze and excitation (SE) ratio (0-1)
+        expand_ratio: expansion ratio (default 1)
+        fused: eliminates depthwise convolution layer
+
+    References:
+        [1] https://arxiv.org/pdf/2104.00298.pdf (EfficientNetV2)
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size=3,
+                 stride=1,
+                 padding=0,
+                 bias=False,
+                 se_ratio=None, 
+                 expand_ratio=1,
+                 fused=False,
+                 **kwargs):
+        super().__init__()
+        
+        self.has_se = (se_ratio is not None) and (0 < se_ratio <= 1)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.expand_ratio = expand_ratio
+        self.fused = fused
+  
+        # Expansion phase (Inverted Bottleneck)
+        inp = in_channels  # number of input channels
+        out = in_channels * expand_ratio  # number of output channels
+        if expand_ratio != 1:
+            if fused is True:
+              self.expand_conv = FusedConv2dBNReLU(inp, out, kernel_size=kernel_size, padding=1, batchnorm='Affine', bias=bias, eps=1e-03, momentum=0.01)
+            else:
+              self.expand_conv = FusedConv2dBNReLU(inp, out, 1, batchnorm='Affine', bias=bias, eps=1e-03, momentum=0.01)
+            
+        # Depthwise Convolution phase
+        if fused is not True:
+          self.depthwise_conv = FusedConv2dBNReLU(
+              in_channels=out, out_channels=out, groups=out, padding=1,# groups makes it depthwise
+              kernel_size=kernel_size, stride=stride, batchnorm='Affine', bias=bias, eps=1e-03, momentum=0.01, **kwargs)
+
+        # Squeeze and Excitation phase
+        if self.has_se:
+            num_squeezed_channels = max(1, int(in_channels * se_ratio))
+            self.se_reduce = FusedConv2dReLU(in_channels=out, out_channels=num_squeezed_channels, kernel_size=1, stride=1, bias=bias, **kwargs)
+            self.se_expand = Conv2d(in_channels=num_squeezed_channels, out_channels=out, kernel_size=1, stride=1, bias=bias, **kwargs)
+
+        # Output Convolution phase
+        final_out = out_channels
+        self.project_conv = FusedConv2dBN(in_channels=out, out_channels=final_out, kernel_size=1, batchnorm='Affine', bias=bias, eps=1e-03, momentum=0.01, **kwargs)
+
+        # Skip connection
+        self.resid = Add()
+
+    def forward(self, inputs):
+        """MBConvBlock's forward function.
+
+        Args:
+            inputs (tensor): Input tensor.
+        Returns:
+            Output of this block after processing.
+        """
+
+        # Expansion Convolution layer
+        x = inputs
+        if self.expand_ratio != 1:
+            x = self.expand_conv(inputs)
+
+        # Depthwise Convolution layer
+        if self.fused is not True:
+          x = self.depthwise_conv(x)
+
+        # Squeeze and Excitation layers
+        if self.has_se:
+            x_squeezed = F.adaptive_avg_pool2d(x, 1)
+            x_squeezed = self.se_reduce(x_squeezed)
+            x_squeezed = self.se_expand(x_squeezed)
+            x = torch.sigmoid(x_squeezed) * x
+        
+        # Output Convolution layer
+        x = self.project_conv(x)
+
+        # Skip connection
+        input_filters, output_filters = self.in_channels, self.out_channels
+        if self.stride == 1 and input_filters == output_filters:
+            x = self.resid(x, inputs)
+        return x
+       
+
---
+++ scripts/evaluate_cifar100_effnet2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 train.py --model ai87effnetv2 --dataset CIFAR100 --evaluate --device MAX78000 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-cifar100-effnet2-qat8-q.pth.tar -8 --use-bias "$@"
