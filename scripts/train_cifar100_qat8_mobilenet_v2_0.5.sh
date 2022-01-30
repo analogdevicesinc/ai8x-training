d99b5fa36c93168d1fc932de91f4e2ab39233750
Erman Okman <Erman.Okman@maximintegrated.com>
2022-01-30 09:13:34 +0300

    Linter fix

--- scripts/train_cifar100_qat8_mobilenet_v2_0.5.sh
+++ scripts/train_cifar100_qat8_mobilenet_v2_0.5.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 python train.py --deterministic --epochs 300 --optimizer SGD --lr 0.1 --compress schedule-cifar100-mobilenetv2.yaml --model ai87netmobilenetv2cifar100_m0_5 --dataset CIFAR100 --device MAX78002 --batch-size 128 --print-freq 100 --validation-split 0 --use-bias --qat-policy qat_policy_cifar100_mobilenetv2.yaml "$@"
