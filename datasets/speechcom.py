9eb446317576e04e281a53dd44a4cc1db0c44134
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 01:04:29 +0000

    Adding tarfile member sanitization to extractall()

--- datasets/speechcom.py
+++ datasets/speechcom.py
@@ -239,7 +239,26 @@ class SpeechCom(torch.utils.data.Dataset):
 
         if from_path.endswith('.tar.gz'):
             with tarfile.open(from_path, 'r:gz') as tar:
-                tar.extractall(path=to_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=to_path)
         else:
             raise ValueError(f"Extraction of {from_path} not supported")
 
