analogdevicesinc/ai8x-training

commit a0721b54905769ec869c02a5c8b3f7151ec96ccc

Use torch operations instead of Python for training speed increase (#210)
* Replace Python exp/mul/div  with torch for tensors

ai8x.py
@@ -252,8 +252,8 @@ class Scaler(nn.Module):
     def forward(self, x, s):  # pylint: disable=arguments-differ
         """Forward prop"""
         if dev.simulate:
-            return FloorFunction.apply(x*s)
-        return x*s
+            return FloorFunction.apply(x.mul(s))
+        return x.mul(s)
 
 
 class ScalerONNX(nn.Module):
@@ -265,7 +265,7 @@ class ScalerONNX(nn.Module):
         """Forward prop"""
         if dev.simulate:
             return x.mul(s).floor()
-        return x*s
+        return x.mul(s)
 
 
 class RoundQat(nn.Module):
@@ -445,7 +445,7 @@ class WeightScale(nn.Module):
     """
     def forward(self, x):  # pylint: disable=arguments-differ
         """Forward prop"""
-        return 2.**(-x)
+        return torch.exp2(-x)
 
 
 class OutputScale(nn.Module):
@@ -454,7 +454,7 @@ class OutputScale(nn.Module):
     """
     def forward(self, x):  # pylint: disable=arguments-differ
         """Forward prop"""
-        return 2.**x
+        return torch.exp2(x)
 
 
 class Abs(nn.Module):
@@ -594,11 +594,11 @@ class QuantizationAwareModule(nn.Module):
 
             weights = self.op.weight.data
             self.op.weight.data = \
-                self.clamp_weight(self.quantize_weight(weight_scale * self.op.weight))
+                self.clamp_weight(self.quantize_weight(self.op.weight.mul(weight_scale)))
             if self.op.bias is not None:
                 biases = self.op.bias.data
                 self.op.bias.data = \
-                    self.clamp_bias(self.quantize_bias(weight_scale * self.op.bias))
+                    self.clamp_bias(self.quantize_bias(self.op.bias.mul(weight_scale)))
 
             x = self.op(x)
 
@@ -607,7 +607,7 @@ class QuantizationAwareModule(nn.Module):
                 self.op.bias.data = biases
 
             if self.bn is not None:
-                x = self.bn(x) / 4.
+                x = self.bn(x).div(4.)
             if not self.wide:
                 # The device does not apply output shift in wide mode
                 x = self.scale(x, out_scale)

commit a40e732acfd0e6ae5259eb4bd6edf73fe092eea5

Restore ONNX export (fixes regression caused by #210) (#212)
* Restore ONNX export, regression from #210
* Fix typo in aisegment training script

ai8x.py
@@ -450,6 +450,15 @@ class WeightScale(nn.Module):
         return torch.exp2(-x)
 
 
+class WeightScaleONNX(nn.Module):
+    """
+    Calculate the weight scale (reciprocal of 2 to the power of the output shift)
+    """
+    def forward(self, x):  # pylint: disable=arguments-differ
+        """Forward prop"""
+        return 2.**(-x)
+
+
 class OutputScale(nn.Module):
     """
     Calculate the output scale (2 to the power of the output shift)
@@ -457,6 +466,15 @@ class OutputScale(nn.Module):
     def forward(self, x):  # pylint: disable=arguments-differ
         """Forward prop"""
         return torch.exp2(x)
+
+
+class OutputScaleONNX(nn.Module):
+    """
+    Calculate the output scale (2 to the power of the output shift)
+    """
+    def forward(self, x):  # pylint: disable=arguments-differ
+        """Forward prop"""
+        return 2.**x
 
 
 class Abs(nn.Module):
@@ -1713,7 +1731,11 @@ def onnx_export_prep(m, simplify=False):
     def _onnx_export_prep(m):
         for attr_str in dir(m):
             target_attr = getattr(m, attr_str)
-            if not simplify:
+            if isinstance(target_attr, WeightScale):
+                setattr(m, attr_str, WeightScaleONNX())
+            elif isinstance(target_attr, OutputScale):
+                setattr(m, attr_str, OutputScaleONNX())
+            elif not simplify:
                 if isinstance(target_attr, Quantize):
                     setattr(m, attr_str, QuantizeONNX(target_attr.num_bits))
                 elif isinstance(target_attr, FloorQat):

scripts/train_aisegment_unet.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --deterministic --pr-curves --epochs 200 --optimizer Adam --lr 0.001 --wd 0 --model ai85unetlarge --out-fold-ratio 4 --use-bias --dataset AISegment_352 --device MAX78000 --batch-size 32 --qat-policy policies/qat_policy_aisegment.yaml --compress policies/schedule-aisegment.yaml --validation-split 0 --print_freq 250 "$@"
+python train.py --deterministic --pr-curves --epochs 200 --optimizer Adam --lr 0.001 --wd 0 --model ai85unetlarge --out-fold-ratio 4 --use-bias --dataset AISegment_352 --device MAX78000 --batch-size 32 --qat-policy policies/qat_policy_aisegment.yaml --compress policies/schedule-aisegment.yaml --validation-split 0 --print-freq 250 "$@"