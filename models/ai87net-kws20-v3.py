829dce19a049f813b08e4bc8adb8495ae8eb1b7c
Afshin Niktash <48036488+aniktash@users.noreply.github.com>
2023-02-17 08:35:20 -0800

    Add files for training and evaluation of kws20 on MAX78002 (#213)

---
+++ models/ai87net-kws20-v3.py
@@ -0,0 +1,96 @@
+###################################################################################################
+#
+# Copyright (C) 2020-2023 Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+"""
+Keyword spotting network for AI87
+"""
+from torch import nn
+
+import ai8x
+
+
+class AI87KWS20Netv3(nn.Module):
+    """
+    Compound KWS20 v3 Audio net, all with Conv1Ds
+    """
+
+    # num_classes = n keywords + 1 unknown
+    def __init__(
+            self,
+            num_classes=21,
+            num_channels=128,
+            dimensions=(128, 1),  # pylint: disable=unused-argument
+            bias=False,
+            **kwargs
+
+    ):
+        super().__init__()
+        self.drop = nn.Dropout(p=0.2)
+        # Time: 128 Feature :128
+        self.voice_conv1 = ai8x.FusedConv1dReLU(num_channels, 100, 1, stride=1, padding=0,
+                                                bias=bias, **kwargs)
+        # T: 128 F: 100
+        self.voice_conv2 = ai8x.FusedConv1dReLU(100, 96, 3, stride=1, padding=0,
+                                                bias=bias, **kwargs)
+        # T: 126 F : 96
+        self.voice_conv3 = ai8x.FusedMaxPoolConv1dReLU(96, 64, 3, stride=1, padding=0,
+                                                       bias=bias, **kwargs)
+        # T: 62 F : 64
+        self.voice_conv4 = ai8x.FusedConv1dReLU(64, 48, 3, stride=1, padding=0,
+                                                bias=bias, **kwargs)
+        # T : 60 F : 48
+        self.kws_conv1 = ai8x.FusedMaxPoolConv1dReLU(48, 64, 3, stride=1, padding=0,
+                                                     bias=bias, **kwargs)
+        # T: 30 F : 64
+        self.kws_conv2 = ai8x.FusedConv1dReLU(64, 96, 3, stride=1, padding=0,
+                                              bias=bias, **kwargs)
+        # T: 28 F : 96
+        self.kws_conv3 = ai8x.FusedAvgPoolConv1dReLU(96, 100, 3, stride=1, padding=0,
+                                                     bias=bias, **kwargs)
+        # T : 14 F: 100
+        self.kws_conv4 = ai8x.FusedMaxPoolConv1dReLU(100, 64, 3, stride=1, padding=0,
+                                                     bias=bias, **kwargs)
+        # T : 7 F: 128
+        self.fc = ai8x.Linear(192, num_classes, bias=bias, wide=True, **kwargs)
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        """Forward prop"""
+        # Run CNN
+        x = self.voice_conv1(x)
+        x = self.voice_conv2(x)
+        x = self.drop(x)
+        x = self.voice_conv3(x)
+        x = self.voice_conv4(x)
+        x = self.drop(x)
+        x = self.kws_conv1(x)
+        x = self.kws_conv2(x)
+        x = self.drop(x)
+        x = self.kws_conv3(x)
+        x = self.kws_conv4(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def ai87kws20netv3(pretrained=False, **kwargs):
+    """
+    Constructs a AI87KWS20Net model.
+    rn AI87KWS20Net(**kwargs)
+    """
+    assert not pretrained
+    return AI87KWS20Netv3(**kwargs)
+
+
+models = [
+    {
+        'name': 'ai87kws20netv3',
+        'min_input': 1,
+        'dim': 1,
+    },
+]
---
+++ scripts/train_kws20_v3_ai87.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 train.py --epochs 200 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule_kws20.yaml --model ai87kws20netv3 --dataset KWS_20 --confusion --device MAX78002 "$@"
