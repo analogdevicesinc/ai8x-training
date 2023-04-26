67d1ffd6c52b302210cdfc556bc062f575787345
Robert Muchsel <robert.muchsel@maximintegrated.com>
2023-04-26 14:02:12 -0500

    Reduce initial learning rate for SGD example scripts

--- scripts/train_mnist.sh
+++ scripts/train_mnist.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --lr 0.1 --optimizer SGD --epochs 200 --deterministic --compress policies/schedule.yaml --model ai85net5 --dataset MNIST --confusion --param-hist --pr-curves --embedding --device MAX78000 "$@"
+python train.py --lr 0.01 --optimizer SGD --epochs 200 --deterministic --compress policies/schedule.yaml --model ai85net5 --dataset MNIST --confusion --param-hist --pr-curves --embedding --device MAX78000 "$@"
--- scripts/train_mnist_qat.sh
+++ scripts/train_mnist_qat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --lr 0.1 --optimizer SGD --epochs 200 --deterministic --seed 1 --compress policies/schedule.yaml --model ai85net5 --dataset MNIST --confusion --param-hist --pr-curves --embedding --device MAX78000 --qat-policy policies/qat_policy_mnist.yaml "$@"
+python train.py --lr 0.01 --optimizer SGD --epochs 200 --deterministic --seed 1 --compress policies/schedule.yaml --model ai85net5 --dataset MNIST --confusion --param-hist --pr-curves --embedding --device MAX78000 --qat-policy policies/qat_policy_mnist.yaml "$@"
