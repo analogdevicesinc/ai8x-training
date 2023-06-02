6c0304ba14bc660c83c4f819eb52daffcd4100d3
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-06-02 16:49:11 +0300

    Update pass_fail.py

--- regression-test/pass_fail.py
+++ regression-test/pass_fail.py
@@ -51,12 +51,13 @@ def check_top_value(file, threshold, map_value):
             print(f"\033[31m\u2718\033[0m Test failed for {model_name} since"
                   f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
             return False
-        print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
-              f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
-        return True
+             
+        else:
+            print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
+                  f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
+            return True
     if map_value:
         with open(file, 'r', encoding='utf-8') as f:
-
             model_name = file.split('/')[-1].split('___')[0]
             # Read all lines in the file
             lines = f.readlines()
@@ -70,9 +71,11 @@ def check_top_value(file, threshold, map_value):
             print(f"\033[31m\u2718\033[0m Test failed for {model_name} since"
                   f" mAP value changed {top1_diff} % at {epoch_num}th epoch.")
             return False
-        print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
-              f" mAP value changed {top1_diff} % at {epoch_num}th epoch.")
-        return True
+
+        else:
+            print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
+                  f" mAP value changed {top1_diff} % at {epoch_num}th epoch.")
+            return True
 
 
 passing = []
