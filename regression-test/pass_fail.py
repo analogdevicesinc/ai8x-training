c29ff4289ce772a384403f663f90d1decd257a68
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-06-07 13:12:51 +0300

    Update pass_fail.py

--- regression-test/pass_fail.py
+++ regression-test/pass_fail.py
@@ -56,7 +56,7 @@ def check_top_value(file, threshold, map_value):
         print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
               f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
         return True
-    else :
+    else:
         with open(file, 'r', encoding='utf-8') as f:
 
             model_name = file.split('/')[-1].split('___')[0]
