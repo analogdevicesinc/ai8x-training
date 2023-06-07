aa0c58e351512b185b366ce7c2d1ba10bd0b8665
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-06-07 12:58:24 +0300

    Update pass_fail.py

--- regression-test/pass_fail.py
+++ regression-test/pass_fail.py
@@ -11,6 +11,7 @@ Check the test results
 """
 import argparse
 import os
+import sys
 
 import yaml
 
@@ -52,11 +53,10 @@ def check_top_value(file, threshold, map_value):
             print(f"\033[31m\u2718\033[0m Test failed for {model_name} since"
                   f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
             return False
-        else:
-            print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
-                  f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
-            return True
-    if map_value:
+        print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
+              f" Top1 value changed {top1_diff} % at {epoch_num}th epoch.")
+        return True
+    else :
         with open(file, 'r', encoding='utf-8') as f:
 
             model_name = file.split('/')[-1].split('___')[0]
@@ -72,10 +72,9 @@ def check_top_value(file, threshold, map_value):
             print(f"\033[31m\u2718\033[0m Test failed for {model_name} since"
                   f" mAP value changed {top1_diff} % at {epoch_num}th epoch.")
             return False
-        else:
-            print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
-                  f" mAP value changed {top1_diff} % at {epoch_num}th epoch.")
-            return True
+        print(f"\033[32m\u2714\033[0m Test passed for {model_name} since"
+              f" mAP value changed {top1_diff} % at {epoch_num}th epoch.")
+        return True
 
 
 passing = []
@@ -98,4 +97,4 @@ for logs in sorted(os.listdir(log_path)):
 
 if not all(passing):
     print("\033[31mAll tests did not passed. Cancelling github actions.")
-    exit(1)
+    sys.exit(1)
