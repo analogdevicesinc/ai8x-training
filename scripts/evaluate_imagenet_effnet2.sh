e294a7d8e827ae03608ea63146289ce11edbaca3
Robert Muchsel <robert.muchsel@analog.com>
2023-09-12 16:28:43 -0500

    Ensure all scripts call python instead of python3

--- scripts/evaluate_imagenet_effnet2.sh
+++ scripts/evaluate_imagenet_effnet2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 train.py --model ai87imageneteffnetv2 --dataset ImageNet --evaluate --device MAX78002 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-imagenet-effnet2-q.pth.tar -8 --use-bias "$@"
+python train.py --model ai87imageneteffnetv2 --dataset ImageNet --evaluate --device MAX78002 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-imagenet-effnet2-q.pth.tar -8 --use-bias "$@"
