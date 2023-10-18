7eb510774e8880a6deda9e690a5f96af24aecc31
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-10-18 20:50:34 +0300

    Add evaluation test to regression testing (#255)

--- regression/create_onnx_script.py
+++ regression/create_onnx_script.py
@@ -108,7 +108,6 @@ with open(output_file_path, "w", encoding='utf-8') as onnx_scripts:
             modelsearch = element[-4][3:]
             datasearch = element[-3].split('_')[0]
             if datasearch == dataset.split('_')[0] and modelsearch == model:
-                # model_paths.remove(tar)
                 tar_path = tar
                 timestamp = time_stamp()
                 temp = (
