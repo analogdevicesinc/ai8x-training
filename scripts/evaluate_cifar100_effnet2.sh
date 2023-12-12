0c35aff2c044569d171e1702f2ee646d0cdf72d6
asyatrhl <123384000+asyatrhl@users.noreply.github.com>
2023-12-13 00:48:26 +0300

    Update evaluation scripts (#277)

--- scripts/evaluate_cifar100_effnet2.sh
+++ scripts/evaluate_cifar100_effnet2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --model ai87effnetv2 --dataset CIFAR100 --evaluate --device MAX78000 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-cifar100-effnet2-qat8-q.pth.tar -8 --use-bias "$@"
+python train.py --model ai87effnetv2 --dataset CIFAR100 --evaluate --device MAX78002 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-cifar100-effnet2-qat8-q.pth.tar -8 --use-bias "$@"
--- scripts/evaluate_imagenet_effnet2.sh
+++ scripts/evaluate_imagenet_effnet2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python train.py --model ai87imageneteffnetv2 --dataset ImageNet --evaluate --device MAX78002 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-imagenet-effnet2-q.pth.tar -8 --use-bias "$@"
+python train.py --model ai87imageneteffnetv2 --dataset ImageNet --evaluate --device MAX78002 --exp-load-weights-from ../ai8x-synthesis/trained/ai87-imagenet-effnet2-q.pth.tar -8 --use-bias --qat-policy policies/qat_policy_imagenet.yaml "$@"
