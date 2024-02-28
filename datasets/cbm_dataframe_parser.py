4ca97464bae40b4cc3e79dd28e7fb9e7a86b7f31
asyaturhal <asya.turhal@analog.com>
2024-02-28 22:48:48 +0300

    Linter fix

--- datasets/cbm_dataframe_parser.py
+++ datasets/cbm_dataframe_parser.py
@@ -205,7 +205,7 @@ class CbM_DataFrame_Parser(Dataset):  # pylint: disable=too-many-instance-attrib
         self.cnn_1dinput_len = cnn_1dinput_len
         assert self.cnn_1dinput_len >= (self.target_sampling_rate_Hz *
                                         self.signal_duration_in_sec)/2, \
-                                            'CNN input length is incorrect.'
+                                        'CNN input length is incorrect.'
 
         if not isinstance(self.downsampling_ratio, int) or self.downsampling_ratio < 1:
             raise ValueError(
