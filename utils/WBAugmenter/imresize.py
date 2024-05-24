7fa07c9178390a3ce9f27da503cf60840e2297cd
Robert Muchsel <16564465+rotx-maxim@users.noreply.github.com>
2024-05-24 09:21:52 -0500

    Minor fixes

--- utils/WBAugmenter/imresize.py
+++ utils/WBAugmenter/imresize.py
@@ -2,7 +2,7 @@
 # MIT License
 #
 # Copyright (c) 2020 Alex
-# Portions Copyright (C) 20242024 Analog Devices, Inc.
+# Portions Copyright (C) 2024 Analog Devices, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
 """
 Python implementation of MATLAB's image resize function imresize()
 Source: https://github.com/fatheral/matlab_imresize
